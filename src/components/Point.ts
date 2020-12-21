ba5fbec3e594ffe3c502112b7f98d581b2b20353
gduk3 <phoenix.ofdarkness@mail.ru>
2020-12-21 23:24:20 +0500

    Point Class implementation of methods

--- src/components/Point.ts
+++ src/components/Point.ts
@@ -2,11 +2,24 @@ class Point implements IPoint {
 	private x: number;
 	private y: number;
 
-	constructor(x: number, y: number) {}
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
 
-	setPosition(x: number, y: number): void {}
-	getPosition(): { x: number; y: number } {}
-	getDistance(point: IPoint): number {}
+	setPosition(x: number, y: number): void {
+		this.x = x;
+		this.y = y;
+	}
+	getPosition(): { x: number; y: number } {
+		return { x: this.x, y: this.y };
+	}
+	getDistance(point: IPoint): number {
+		const pointPosition = point.getPosition();
+		return Math.sqrt(
+			Math.pow(pointPosition.x - this.x, 2) + Math.pow(pointPosition.y - this.y, 2)
+		);
+	}
 }
 
 export default Point;
