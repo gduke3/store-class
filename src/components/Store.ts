daf2bde229766ac92cf5bd0fb8c482dd8f55e675
gduk3 <phoenix.ofdarkness@mail.ru>
2020-12-21 22:55:01 +0500

    Store interface declaration

--- src/components/Store.ts
+++ src/components/Store.ts
@@ -1,1 +1,18 @@
-export {};
+class Store<T> implements IStore<T> {
+	private storage: (T | null)[] = [];
+
+	getSize(): number {}
+	setByIndex(index: number, item: T): void {}
+	getByIndex(index: number): T | null {}
+	getAll(): T[] {}
+	removeByIndex(index: number): boolean {}
+}
+
+export default Store;
+export interface IStore<T> {
+	setByIndex: (index: number, item: T) => void;
+	getByIndex: (index: number) => T | null;
+	removeByIndex: (index: number) => boolean;
+	getAll: () => T[];
+	getSize: () => number;
+}
