3ab2be2a5294fc7c4e058e4affb36585c41f3cd3
gduk3 <phoenix.ofdarkness@mail.ru>
2020-12-21 22:58:10 +0500

    Store Class implementation of methods

--- src/components/Store.ts
+++ src/components/Store.ts
@@ -1,11 +1,25 @@
 class Store<T> implements IStore<T> {
 	private storage: (T | null)[] = [];
 
-	getSize(): number {}
-	setByIndex(index: number, item: T): void {}
-	getByIndex(index: number): T | null {}
-	getAll(): T[] {}
-	removeByIndex(index: number): boolean {}
+	getSize(): number {
+		return this.getAll().length;
+	}
+	setByIndex(index: number, item: T): void {
+		this.storage[index] = item;
+	}
+	getByIndex(index: number): T | null {
+		return this.storage[index] ?? null;
+	}
+	getAll(): T[] {
+		return this.storage.filter((item) => item !== null) as T[];
+	}
+	removeByIndex(index: number): boolean {
+		if (this.storage[index]) {
+			this.storage[index] = null;
+			return true;
+		}
+		return false;
+	}
 }
 
 export default Store;
