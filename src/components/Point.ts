4b19bb38c215be2ea97902d85d9912ff022ca07d
gduk3 <phoenix.ofdarkness@mail.ru>
2020-12-21 23:23:37 +0500

    Point interface declaration

--- src/components/Point.ts
+++ src/components/Point.ts
@@ -1,1 +1,17 @@
-export {};
+class Point implements IPoint {
+	private x: number;
+	private y: number;
+
+	constructor(x: number, y: number) {}
+
+	setPosition(x: number, y: number): void {}
+	getPosition(): { x: number; y: number } {}
+	getDistance(point: IPoint): number {}
+}
+
+export default Point;
+export interface IPoint {
+	setPosition: (x: number, y: number) => void;
+	getPosition: () => { x: number; y: number };
+	getDistance: (point: IPoint) => number;
+}
